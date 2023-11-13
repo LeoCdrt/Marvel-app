9d54d077121c49c11dc17d4896e756f50613e67c
LeoCdrt <leo.coudret@etu.univ-poitiers.fr>
2023-11-13 17:36:21 +0100

    Update

--- src/components/NumberOfCharacters.js
+++ src/components/NumberOfCharacters.js
@@ -1,7 +1,7 @@
 export function NumberOfCharacters({ characters = [] }) {
   return (
     <p>
-      Il y a {characters.length === 0 ? 'no' : characters.length} character{characters.length !== 1 ? 's' : ''}
+      There is {characters.length === 0 ? 'no' : characters.length} character{characters.length !== 1 ? 's' : ''}
     </p>
   );
 }
